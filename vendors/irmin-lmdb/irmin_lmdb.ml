6913a4f7d2f010da5baf12629fadd890531dbbd9
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-02-01 19:21:49 +0100

    lolilol

--- vendors/irmin-lmdb/irmin_lmdb.ml
+++ vendors/irmin-lmdb/irmin_lmdb.ml
@@ -912,7 +912,7 @@ module Make
 
     let promote msg t ?old k =
       print_message t;
-      Fmt.epr "Promote %a.\n%!" H.pp (H.of_raw (Cstruct.of_string k)) ;
+
       (match old with
        | Some _ -> old
        | None   -> raw_find t.old_db k (fun x -> Ok x))
