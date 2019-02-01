37a69b98f7b51783dc4d72ab6119ec7b75665f7a
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-02-01 19:57:39 +0100

    lolilol

--- vendors/irmin-lmdb/irmin_lmdb.ml
+++ vendors/irmin-lmdb/irmin_lmdb.ml
@@ -969,6 +969,7 @@ module Make
       | true -> Lwt.return ()
       | false ->
         Tbl.add context.gc.tbl k' ;
+        Fmt.kstrf ignore "Scan %a.\n%!" H.pp value.key ;
         P.XNode.find_v context.gc.old_db value.key >|= Option.get >>= fun (_, v) ->
         let children = P.Node.Val.list v in
         incr_nodes context.gc.stats ;
